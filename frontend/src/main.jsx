96d012c343cd8e1311222004a4c063683f0fdcd4
Hrishi-524 <sp3567208@gmail.com>
2025-08-27 18:05:14 +0530

    added vercel origin v5

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,7 +7,9 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import { BrowserRouter } from 'react-router-dom';
+import { setupAxiosInterceptors } from './utils/auth.js';
 
+setupAxiosInterceptors()
 
 createRoot(document.getElementById('root')).render(
   //<StrictMode>
